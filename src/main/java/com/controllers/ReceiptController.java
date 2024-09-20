8f65641494947bf1be53234e936dd7634e2c3362
Nguyễn Thành Nhân <nhanntpc06420@gmail.com>
2024-09-20 14:35:30 +0700

    Done API Suppliers and Receipt

--- src/main/java/com/controllers/ReceiptController.java
+++ src/main/java/com/controllers/ReceiptController.java
@@ -1,6 +1,7 @@
 package com.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,15 +12,20 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.entities.User;
 import com.errors.ApiResponse;
 import com.errors.FieldErrorDTO;
 import com.models.ReceiptCreateDTO;
 import com.models.ReceiptDTO;
+import com.services.AuthService;
+import com.services.JWTService;
 import com.services.ReceiptService;
+import com.services.UserService;
 import com.utils.ValidationUtil;
 
 import jakarta.validation.Valid;
@@ -31,9 +37,57 @@ public class ReceiptController {
 	@Autowired
 	private ReceiptService warehouseService;
 
+	@Autowired
+	private AuthService authService;
+
+	@Autowired
+	private JWTService jwtService;
+
+	@Autowired
+	private UserService userService;
+
 	@GetMapping
 	public ResponseEntity<ApiResponse<?>> getAllWarehouses(@RequestParam(value = "page", defaultValue = "0") int page,
-			@RequestParam(value = "size", defaultValue = "5") int size) {
+			@RequestParam(value = "size", defaultValue = "5") int size,
+			@RequestHeader("Authorization") Optional<String> authHeader) {
+
+		ApiResponse<String> errorResponse = new ApiResponse<>();
+
+		if (!authHeader.isPresent()) {
+			errorResponse.setErrorCode(400);
+			errorResponse.setMessage("Authorization header is missing");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+		}
+
+		String token = authService.readTokenFromHeader(authHeader);
+
+		try {
+			jwtService.extractUsername(token);
+		} catch (Exception e) {
+			errorResponse.setErrorCode(400);
+			errorResponse.setMessage("Invalid token format");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+		}
+
+		if (jwtService.isTokenExpired(token)) {
+			errorResponse.setErrorCode(401);
+			errorResponse.setMessage("Token expired");
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+		}
+
+		String username = jwtService.extractUsername(token);
+		User user = userService.getUserByUsername(username);
+		if (user == null) {
+			errorResponse.setErrorCode(404);
+			errorResponse.setMessage("Account not found");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+		}
+
+		if (user.getStatus() == 0) {
+			errorResponse.setErrorCode(403);
+			errorResponse.setMessage("Account locked - Username: " + username);
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
+		}
 
 		Page<ReceiptDTO> receiptDTOPage = warehouseService.getAllWarehouses(page, size);
 
@@ -50,11 +104,50 @@ public class ReceiptController {
 	}
 
 	@GetMapping("/receipt-detail")
-	public ResponseEntity<ApiResponse<ReceiptDTO>> getWarehouseById(@RequestParam Integer id) {
+	public ResponseEntity<ApiResponse<?>> getWarehouseById(@RequestParam Integer id,
+			@RequestHeader("Authorization") Optional<String> authHeader) {
+
+		ApiResponse<String> errorResponse = new ApiResponse<>();
+
+		if (!authHeader.isPresent()) {
+			errorResponse.setErrorCode(400);
+			errorResponse.setMessage("Authorization header is missing");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+		}
+
+		String token = authService.readTokenFromHeader(authHeader);
+
+		try {
+			jwtService.extractUsername(token);
+		} catch (Exception e) {
+			errorResponse.setErrorCode(400);
+			errorResponse.setMessage("Invalid token format");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+		}
+
+		if (jwtService.isTokenExpired(token)) {
+			errorResponse.setErrorCode(401);
+			errorResponse.setMessage("Token expired");
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+		}
+
+		String username = jwtService.extractUsername(token);
+		User user = userService.getUserByUsername(username);
+		if (user == null) {
+			errorResponse.setErrorCode(404);
+			errorResponse.setMessage("Account not found");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+		}
+
+		if (user.getStatus() == 0) {
+			errorResponse.setErrorCode(403);
+			errorResponse.setMessage("Account locked - Username: " + username);
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
+		}
 		ReceiptDTO receiptDTO = warehouseService.getWarehouseById(id);
 
 		if (receiptDTO == null) {
-			ApiResponse<ReceiptDTO> response = new ApiResponse<>(404, "Not Found", null);
+			ApiResponse<ReceiptDTO> response = new ApiResponse<>(404, "Not found receipt", null);
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
 		}
 
@@ -64,16 +157,52 @@ public class ReceiptController {
 
 	@PostMapping
 	public ResponseEntity<ApiResponse<?>> createReceipt(@Valid @RequestBody ReceiptCreateDTO receiptCreateDTO,
-			BindingResult errors) {
+			BindingResult errors, @RequestHeader("Authorization") Optional<String> authHeader) {
 
 		ApiResponse<?> errorResponse = new ApiResponse<>();
+
+		if (!authHeader.isPresent()) {
+			errorResponse.setErrorCode(400);
+			errorResponse.setMessage("Authorization header is missing");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+		}
+
+		String token = authService.readTokenFromHeader(authHeader);
+
+		try {
+			jwtService.extractUsername(token);
+		} catch (Exception e) {
+			errorResponse.setErrorCode(400);
+			errorResponse.setMessage("Invalid token format");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+		}
+
+		if (jwtService.isTokenExpired(token)) {
+			errorResponse.setErrorCode(401);
+			errorResponse.setMessage("Token expired");
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+		}
+
+		String username = jwtService.extractUsername(token);
+		User user = userService.getUserByUsername(username);
+		if (user == null) {
+			errorResponse.setErrorCode(404);
+			errorResponse.setMessage("Account not found");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+		}
+
+		if (user.getStatus() == 0) {
+			errorResponse.setErrorCode(403);
+			errorResponse.setMessage("Account locked - Username: " + username);
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
+		}
 		List<FieldErrorDTO> validationErrors = ValidationUtil.validateErrors(errors);
 		if (!validationErrors.isEmpty()) {
 			errorResponse = new ApiResponse<>(400, "Validation failed.", validationErrors);
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
 		}
 
-		ApiResponse<?> response = warehouseService.createReceipt(receiptCreateDTO);
+		ApiResponse<?> response = warehouseService.createReceipt(receiptCreateDTO, user);
 		return ResponseEntity.ok(response);
 	}
 }
