294e98da3a5dc5e1ce5dadeb7f9255ff6681b0fd
AnTruqhy <hoaithopirate@gmail.com>
2024-11-04 23:44:23 +0700

    Revert "CategoriesController [Minh Ty]"
    This reverts commit ebc9b1951bbe6af09731bc3f2994c6e8fe3e29f1.

--- src/main/java/com/repositories/CategoryJPA.java
+++ src/main/java/com/repositories/CategoryJPA.java
@@ -2,8 +2,6 @@ package com.repositories;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,10 +11,4 @@ import com.entities.Category;
 public interface CategoryJPA extends JpaRepository<Category, Integer> {
 	@Query("SELECT o.category FROM ProductCategory o WHERE  o.product.productId=:productId")
 	public List<Category> getAllCategoryByProduct(@Param("productId") int productId);
-	
-	@Query("SELECT o FROM Category o WHERE o.categoryName =:name")
-	public Category getCategoryByName(@Param("name") String name);
-	
-	@Query("SELECT o FROM Category o WHERE o.categoryName LIKE:keyword")
-	public Page<Category> getAllCategoryByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
