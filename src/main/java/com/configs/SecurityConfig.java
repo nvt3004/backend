40676b0e7deb366ef0dd4711c1c8759f1570c8cd
Ngô Thái <ngothai3004@gmail.com>
2024-12-12 22:40:28 +0700

    cors

--- src/main/java/com/configs/SecurityConfig.java
+++ src/main/java/com/configs/SecurityConfig.java
@@ -117,7 +117,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://stepstothefuture.store","http://103.72.97.191:3000"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		configuration.setAllowCredentials(true);
