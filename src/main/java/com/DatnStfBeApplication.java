f2f2c593fa1a2163aa4ad511b9a29f35a128f241
Ngô Thái <ngothai3004@gmail.com>
2024-12-17 16:08:22 +0700

    push be

--- src/main/java/com/DatnStfBeApplication.java
+++ src/main/java/com/DatnStfBeApplication.java
@@ -1,4 +1,6 @@
 package com;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class DatnStfBeApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(DatnStfBeApplication.class, args);
 	}
 
